c62dd31a98626fec415a05720bcbae365f2eab5e
Nick <54389956+NickAlvarez20@users.noreply.github.com>
2025-08-01 03:01:18 -0700

    Create insertionSort.js

---
+++ insertionSort.js
@@ -0,0 +1,26 @@
+var insert = function(array, rightIndex, value) {
+    for(var j = rightIndex;
+        j >= 0 && array[j] > value;
+        j--) {
+        array[j + 1] = array[j];
+    }   
+    array[j + 1] = value; 
+};
+
+var insertionSort = function(array) {
+    for(var i = 1; i < array.length; i++) {
+    insert(array, i-1, array[i]);
+    
+    }
+    
+};
+
+var array = [22, 11, 99, 88, 9, 7, 42];
+insertionSort(array);
+println("Array after sorting:  " + array);
+Program.assertEqual(array, [7, 9, 11, 22, 42, 88, 99]);
+
+var array = [4, 57, 13, 99, 800, 33, 50];
+insertionSort(array);
+println("Array after sorting: " + array);
+Program.assertEqual(array, [4, 13, 33, 50, 57, 99, 800]);
